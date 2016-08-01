83a087109ec08bcd6cc2d9c51514f45ff47ba327
FellipePeixoto <rochapeduardo@gmail.com>
2016-07-31 23:37:46 -0300

    Grafo beta v1.0 (bugada)

--- NetBeansProjects/grafo_parte2/main.c
+++ NetBeansProjects/grafo_parte2/main.c
@@ -14,34 +14,6 @@
  * 
  */
 int main(int argc, char** argv) {
-    
-    Grafo *graf = GGcarregaGrafo("grafo1.txt");
-    
-    GVdestroiVertice(graf,1);
-    
-    TipoAresta *aux;
-    
-    printf("Arestas\n");
-    for(aux= graf->pa;aux!=NULL;aux=aux->prox)
-    {
-        printf("%i %i %i\n",aux->id,aux->origem->id,aux->destino->id);
-    }
-    
-    printf("||\n");
-    
-    TipoVertice *aux1;
-    TipoEstrela *aux2;
-    printf("Vertices\n");
-    for(aux1= graf->pv;aux1!=NULL;aux1=aux1->prox)
-    {
-        printf("%i -",aux1->id);
-        for(aux2=aux1->estrela;aux2!=NULL;aux2=aux2->prox)
-        {
-            printf(" %i ",aux2->aresta->id);
-        }
-        printf("\n");
-    }
-    
-        
+           
     return (EXIT_SUCCESS);
 }
